3ad51400dd7812008b4c43301f54d5c128fa5bd5
KrogeChen <KrogeChen@qq.com>
2020-06-13 08:57:32 +0800

    frist add files

---
+++ sofrware/tester_ctrl/README.md
@@ -0,0 +1,8 @@
+## 主控板软件
+MCU: STM32F103RCT6
+edit: vscode
+
+
+
+
+
---
+++ sofrware/tester_ctrl/prj/app/app_cfg.h
@@ -0,0 +1,7 @@
+#ifndef app_cfg_H
+#define app_cfg_H
+
+#include ".\app_general.h"
+#include "..\.\pbc\pbc_sysTick\pbc_sysTick.h"
+
+#endif //app_cfg_H
